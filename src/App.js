abae0cd8a2e13c40f48efe563fc8bd6bea96badf
Brook Hatia <110070986+brook-hatia@users.noreply.github.com>
2023-05-05 18:41:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,24 +11,26 @@ import Give from "./components/Give.js"
 import SupportEth from "./components/Support-Ethiopia.js"
 import SupportReh from "./components/Support-Rehoboth.js"
 import GiveNow from "./components/GiveNow.js"
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header/>
+    <HashRouter>
       <Routes>
-        <Route path='rehoboth-church/' element=<Home/>/>
-        <Route path='rehoboth-church/about' element=<About/>/>
-        <Route path='rehoboth-church/sermons' element=<Sermons/>/>
-        <Route path='rehoboth-church/events' element=<Events/>/>
-        <Route path='rehoboth-church/ministries/bible-study' element=<BibleStudy/>/>
-        <Route path='rehoboth-church/ministries/kids' element=<Kids/>/>
-        <Route path='rehoboth-church/give' element=<Give/>/>
-        <Route path='rehoboth-church/give/support-ethiopia' element=<SupportEth/>/>
-        <Route path='rehoboth-church/give/support-rehoboth' element=<SupportReh/>/>
-        <Route path='rehoboth-church/give/give-now' element=<GiveNow/>/>
+        <Route path='/rehoboth-church/' element=<Home/>/>
+        <Route path='/rehoboth-church/about' element=<About/>/>
+        <Route path='/rehoboth-church/sermons' element=<Sermons/>/>
+        <Route path='/rehoboth-church/events' element=<Events/>/>
+        <Route path='/rehoboth-church/ministries/bible-study' element=<BibleStudy/>/>
+        <Route path='/rehoboth-church/ministries/kids' element=<Kids/>/>
+        <Route path='/rehoboth-church/give' element=<Give/>/>
+        <Route path='/rehoboth-church/give/support-ethiopia' element=<SupportEth/>/>
+        <Route path='/rehoboth-church/give/support-rehoboth' element=<SupportReh/>/>
+        <Route path='/rehoboth-church/give/give-now' element=<GiveNow/>/>
       </Routes>
+    </HashRouter>
       <Footer/>
     </div>
   );
