8276b906a326b9d22223f6c42312f140337f6816
Brook Hatia <110070986+brook-hatia@users.noreply.github.com>
2023-05-05 17:14:24 -0500

    updated links

--- src/App.js
+++ src/App.js
@@ -19,15 +19,15 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/rehoboth-church/' element=<Home/>/>
-        <Route path='/about' element=<About/>/>
-        <Route path='/sermons' element=<Sermons/>/>
-        <Route path='/events' element=<Events/>/>
-        <Route path='/ministries/bible-study' element=<BibleStudy/>/>
-        <Route path='/ministries/kids' element=<Kids/>/>
-        <Route path='/give' element=<Give/>/>
-        <Route path='/give/support-ethiopia' element=<SupportEth/>/>
-        <Route path='/give/support-rehoboth' element=<SupportReh/>/>
-        <Route path='/give/give-now' element=<GiveNow/>/>
+        <Route path='/rehoboth-church/about' element=<About/>/>
+        <Route path='/rehoboth-church/sermons' element=<Sermons/>/>
+        <Route path='/rehoboth-church/events' element=<Events/>/>
+        <Route path='/rehoboth-church/ministries/bible-study' element=<BibleStudy/>/>
+        <Route path='/rehoboth-church/ministries/kids' element=<Kids/>/>
+        <Route path='/rehoboth-church/give' element=<Give/>/>
+        <Route path='/rehoboth-church/give/support-ethiopia' element=<SupportEth/>/>
+        <Route path='/rehoboth-church/give/support-rehoboth' element=<SupportReh/>/>
+        <Route path='/rehoboth-church/give/give-now' element=<GiveNow/>/>
       </Routes>
       <Footer/>
     </div>
