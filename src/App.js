3995960d2cb9b0d0b10269111976ac11a1298d04
Brook Hatia <110070986+brook-hatia@users.noreply.github.com>
2023-05-05 19:16:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,26 +11,28 @@ import Give from "./components/Give.js"
 import SupportEth from "./components/Support-Ethiopia.js"
 import SupportReh from "./components/Support-Rehoboth.js"
 import GiveNow from "./components/GiveNow.js"
-import { Route, Routes, HashRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header/>
+    <BrowserRouter basename="/rehoboth-church">
     <HashRouter>
       <Routes>
-        <Route path='/rehoboth-church/' element=<Home/>/>
-        <Route path='/rehoboth-church/about' element=<About/>/>
-        <Route path='/rehoboth-church/sermons' element=<Sermons/>/>
-        <Route path='/rehoboth-church/events' element=<Events/>/>
-        <Route path='/rehoboth-church/ministries/bible-study' element=<BibleStudy/>/>
-        <Route path='/rehoboth-church/ministries/kids' element=<Kids/>/>
-        <Route path='/rehoboth-church/give' element=<Give/>/>
-        <Route path='/rehoboth-church/give/support-ethiopia' element=<SupportEth/>/>
-        <Route path='/rehoboth-church/give/support-rehoboth' element=<SupportReh/>/>
-        <Route path='/rehoboth-church/give/give-now' element=<GiveNow/>/>
+        <Route path='/' element=<Home/>/>
+        <Route path='/about' element=<About/>/>
+        <Route path='/sermons' element=<Sermons/>/>
+        <Route path='/events' element=<Events/>/>
+        <Route path='/ministries/bible-study' element=<BibleStudy/>/>
+        <Route path='/ministries/kids' element=<Kids/>/>
+        <Route path='/give' element=<Give/>/>
+        <Route path='/give/support-ethiopia' element=<SupportEth/>/>
+        <Route path='/give/support-rehoboth' element=<SupportReh/>/>
+        <Route path='/give/give-now' element=<GiveNow/>/>
       </Routes>
     </HashRouter>
+    <BrowserRouter/>
       <Footer/>
     </div>
   );
